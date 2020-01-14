fcef75bbde170af316733d0a8f7139a30ff4cc15
ZhdemKatyushu <shiroi.maks@gmail.com>
2020-01-14 13:44:39 +0200

    Added the pages and other changes - Page Object

---
+++ pages/base_page.py
@@ -0,0 +1,12 @@
+﻿from selenium import webdriver
+import pytest
+
+class BasePage():
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(self.url)
+        
+    
